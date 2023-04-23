8bc086f9b29e3dca967ed89168574c16461a772e
ViniciusLimaDeOliveira <61565285+ViniciusLimaDeOliveira@users.noreply.github.com>
2023-04-23 00:29:25 -0300

    65%

--- src/test/java/br/com/americanas/atividade/livraria/controller/PessoaControllerTest.java
+++ src/test/java/br/com/americanas/atividade/livraria/controller/PessoaControllerTest.java
@@ -1,8 +1,10 @@
 package br.com.americanas.atividade.livraria.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,6 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.americanas.atividade.livraria.model.Pessoa;
 import br.com.americanas.atividade.livraria.model.Transacao;
+import br.com.americanas.atividade.livraria.service.EmailValidator;
 
 
 @SpringBootTest
@@ -67,16 +70,42 @@ class PessoaControllerTest extends BasePessoaTest{
  "(99)9-9999-9999", 100D);
      Pessoa pessoaTest = repository.save(pessoaBase);
 
-     assertEquals(pessoaTest,pessoaBase,"ok");
-
      pessoaTest.setNome("PessoaTesteNewName") ;
      pessoaTest.setCpf("1111-1111-111-12");	 
      pessoaTest.setEmail("testeMesmo@gmail.com")	; 
      pessoaTest.setTelefone("(99)9-9999-8888") ;
      pessoaTest.setSaldo(200D) ;
-     assertNotEquals(pessoaTest,pessoaBase,"ok");
+     assertNotEquals(pessoaTest,pessoaBase);
     
      deletarPessoa(pessoaBase);
+    }
+    @Test
+    void testEmailNaoValido() throws Exception{
+        Pessoa pessoa = new Pessoa(101L,"pessoaTeste",
+                "1111-1111-111-11", "testegmail.com",
+                "(99)9-9999-9999", 100D);
+
+        EmailValidator emailValidator = new EmailValidator();
+        assertFalse(emailValidator.validate(pessoa.getEmail()));
+    }
+
+      
+    @Test
+    void testEmailException() throws Exception{
+        
+        Pessoa pessoaBase = criarPessoa(101L,"pessoaTeste",
+        "1111-1111-111-11", "testegmail.com",
+    "(99)9-9999-9999", 100D);
+
+ String response = mvc.perform(MockMvcRequestBuilders.
+    post(baseUri)
+    .content(new ObjectMapper().writeValueAsString(pessoaBase))
+    .contentType(MediaType.APPLICATION_JSON))
+    .andDo(print())
+    .andExpect(status().isBadRequest())
+    .andReturn().getResponse().getErrorMessage();
+
+    assertEquals(response,"Email usado invalido");
     }
   
 }
